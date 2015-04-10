9485c483bf59353f616435df64c5fe6c0beb0ac6
lloydbenson <lloyd.benson@gmail.com>
2015-04-10 11:55:18 -0500

    fix typo

--- bin/start.sh
+++ bin/start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e cfg/ficion_confidence.json ];
 then
-   echo "cfg/ficion_confience.json does not exist.  Please copy from ficion_confidence_example.json"
+   echo "cfg/ficion_confidence.json does not exist.  Please copy from ficion_confidence_example.json"
    exit 2
 else
    node_modules/.bin/rejoice -c cfg/ficion_confidence.json
