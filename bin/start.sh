49d674d7e72a8373550c555a7f43bba872469fdb
lloydbenson <lloyd.benson@gmail.com>
2015-04-15 09:38:38 -0500

    get this working with a separate tacklebox instance

--- bin/start.sh
+++ bin/start.sh
@@ -5,18 +5,20 @@ then
    echo "cfg/gills_confidence.json does not exist.  Please copy from gills_confidence_example.json"
    exit 2
 else
+   if [ ! -e cfg/tacklebox_confidence.json ];
+   then
+      echo "cfg/tacklebox_confidence.json does not exist.  Please copy from tacklebox_confidence_example.json"
+      exit 2
+   fi
    ## generate a gills.json file
    node_modules/.bin/confidence -c cfg/gills_confidence.json --filter.env=dev > cfg/gills.json
    echo "Starting up UI"
    node_modules/.bin/rejoice -c cfg/gills.json > gills_console.log &
-#   for i in {1..2}
-#   if [ ! -e cfg/tacklebox_confidence.json ];
-#   then
-#      echo "cfg/tacklebox_confidence.json does not exist.  Please copy from tacklebox_confidence_example.json"
-#   fi
-#   do
-#      echo "Starting up API instance ${i}"
-#      node_modules/.bin/confidence -c cfg/tacklebox_confidence.json --filter.port=port${i} --filter.env=dev > cfg/tacklebox_port${i}.json
-#      node_modules/.bin/rejoice -c cfg/tacklebox_port${i}.json > tacklebox_port${i}_console.log  &
-#   done
+   #for i in {1..2}
+   for i in 1
+   do
+      echo "Starting up API instance ${i}"
+      node_modules/.bin/confidence -c cfg/tacklebox_confidence.json --filter.port=port${i} --filter.env=dev > cfg/tacklebox_port${i}.json
+      node_modules/.bin/rejoice -c cfg/tacklebox_port${i}.json > tacklebox_port${i}_console.log  &
+   done
 fi
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Stopping UI"
+echo "Stopping API"
 ps auxww | grep rejoice | grep -v grep | awk '{print $2}' | xargs kill
