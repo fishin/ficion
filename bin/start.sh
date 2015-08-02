0707d5bfcf321ce76956b6b1404c31e19c7d5722
lloydbenson <lloyd.benson@gmail.com>
2015-08-02 04:06:13 -0500

    clean up start

--- bin/start.sh
+++ bin/start.sh
@@ -4,21 +4,20 @@ if [ ! -e cfg/gills_confidence.json ];
 then
    echo "cfg/gills_confidence.json does not exist.  Please copy from gills_confidence_example.json"
    exit 2
-else
-   if [ ! -e cfg/tacklebox_confidence.json ];
-   then
-      echo "cfg/tacklebox_confidence.json does not exist.  Please copy from tacklebox_confidence_example.json"
-      exit 2
-   fi
-   ## generate a gills.json file
-   node_modules/.bin/confidence -c cfg/gills_confidence.json --filter.env=dev > cfg/gills.json
-   echo "Starting up UI"
-   node_modules/.bin/rejoice -c cfg/gills.json > gills_console.log &
-   #for i in {1..2}
-   for i in 1
-   do
-      echo "Starting up API instance ${i}"
-      node_modules/.bin/confidence -c cfg/tacklebox_confidence.json --filter.port=port${i} --filter.env=dev > cfg/tacklebox_port${i}.json
-      node_modules/.bin/rejoice -c cfg/tacklebox_port${i}.json > tacklebox_port${i}_console.log  &
-   done
 fi
+if [ ! -e cfg/tacklebox_confidence.json ];
+then
+   echo "cfg/tacklebox_confidence.json does not exist.  Please copy from tacklebox_confidence_example.json"
+   exit 2
+fi
+## generate a gills.json file
+node_modules/.bin/confidence -c cfg/gills_confidence.json --filter.env=dev > cfg/gills.json
+echo "Starting up UI"
+node_modules/.bin/rejoice -c cfg/gills.json > gills_console.log &
+#for i in {1..2}
+for i in 1
+do
+   echo "Starting up API instance ${i}"
+   node_modules/.bin/confidence -c cfg/tacklebox_confidence.json --filter.port=port${i} --filter.env=dev > cfg/tacklebox_port${i}.json
+   node_modules/.bin/rejoice -c cfg/tacklebox_port${i}.json > tacklebox_port${i}_console.log  &
+done
