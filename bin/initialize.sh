e10f2fde9e0b08d0606497ff602193c72ba32f9a
lloydbenson <lloyd.benson@gmail.com>
2015-09-13 15:58:12 -0500

    do 0.10 now

--- bin/initialize.sh
+++ bin/initialize.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-IOJS_VERSION="2.5.0"
+NODEJS_VERSION="0.10.40"
 
 if [ ! -e ~/.nvm/nvm.sh ];
 then
     echo "Installing nvm"
     curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
 else
-   echo "Looking like you have nvm installed"
+   echo "nvm installed"
 fi
 
 source ~/.nvm/nvm.sh
 
 NODE_VERSION=$(node --version)
 
-if [ "${NODE_VERSION}" != "v${IOJS_VERSION}" ]
+if [ "${NODE_VERSION}" != "v${NODEJS_VERSION}" ]
 then
-   echo "Installing iojs"
-   nvm install iojs-${IOJS_VERSION}
+   echo "Installing node ${NODEJS_VERSION}"
+   nvm install ${NODEJS_VERSION}
 else
-   echo "Looks like you have iojs ${IOJS_VERSION} installed"
+   echo "node ${NODEJS_VERSION} installed"
 fi 
